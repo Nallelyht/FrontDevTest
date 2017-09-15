2d2bde604938ee4dc99e80d4c8e67a9da6619940
Nallelyht <you@example.com>
2017-09-15 13:43:55 -0500

    crea funcion anagrama

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,22 @@ $(document).ready(function(){
   $(".button-collapse").sideNav();
   $('.modal').modal();
 });
+/*  se declaran 4 variables dos son anagramas y las otras no*/
+var palabra1= "ana"; 
+var palabra2="naa";
+
+var palabra3="holo";
+var palabra4="hola";
+
+/*se declara funcion para comprar las palabras*/
+var anagrama = function(p1,p2){
+  /*se hace una comparacion de las palabras sean iguales , el metodo .split() divide el split letra por letra, el metodo .sort() las ordena y el metodo .join() las une. En el primer ejemplo de palabras compara aan === aan por que separa, ordena y une las letras si son iguales muestra en consola que es un anagrama, esta funcion solo funciona con letras minusculas*/
+  if (p1.split("").sort().join("") === p2.split("").sort().join("")){
+    console.log("es un anagrama")
+  } else {
+    console.log("no es un anagrama")
+  }
+}
+
+anagrama(palabra1,palabra2);
+anagrama(palabra3, palabra4)
