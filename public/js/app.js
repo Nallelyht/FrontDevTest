040cff779c85202f232155980da18ce36d82d829
Nallelyht <you@example.com>
2017-09-15 13:18:38 -0500

    mauetacion lista

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
-  $('.slider').slider({height: 600, transition:800, interval:7000,});
+  $('.slider').slider({height: 600,transition:800, interval:6000,});
   $(".button-collapse").sideNav();
+  $('.modal').modal();
 });
